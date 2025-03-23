50059d4fa6fc8403dd169cbf22db5779f746367d
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-03-23 03:20:59 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 let ShowModal = (id) => {
-  document.getElementById("overlay").style.display = "block";
-  document.body.classList.remove("relative");
-  document.body.classList.add("fixed");
-  document.getElementById(id).classList.remove("hidden");
   document.getElementById(id).style.transition = "none";
   requestAnimationFrame(function () {
     document.getElementById(id).style.transition = "all .3s ease";
@@ -11,9 +7,6 @@ let ShowModal = (id) => {
   document.getElementById(id).style.pointerEvents = "auto";
 };
 let HidModal = (id) => {
-  document.getElementById("overlay").style.display = "none";
-  document.body.classList.add("relative");
-  document.body.classList.remove("fixed");
   document.getElementById(id).style.transition = "all .3s";
   document.getElementById(id).style.right = "-100%";
   document.getElementById(id).style.pointerEvent = "none";
